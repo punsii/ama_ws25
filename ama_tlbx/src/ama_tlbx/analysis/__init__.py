b91e3934ae106a66f60c1c1cbe0a57d3d443b0df
Jan Duchscherer <duchsche@hm.edu>
2025-10-24 15:36:23 +0200

    feat(analysis): add IQR and Z-score outlier detectors

--- ama_tlbx/src/ama_tlbx/analysis/__init__.py
+++ ama_tlbx/src/ama_tlbx/analysis/__init__.py
@@ -1,8 +1,11 @@
 """Analysis modules for dataset processing and statistical methods."""
 
 from .correlation_analyzer import CorrelationAnalyzer
+from .outlier_detector import IQROutlierDetector, ZScoreOutlierDetector
 
 
 __all__ = [
     "CorrelationAnalyzer",
+    "IQROutlierDetector",
+    "ZScoreOutlierDetector",
 ]
