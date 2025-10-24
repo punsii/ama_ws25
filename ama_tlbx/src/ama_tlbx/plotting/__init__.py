8ddf21a0fb958ae73a65d15f5364d5ffd1c06213
Jan Duchscherer <duchsche@hm.edu>
2025-10-24 15:36:23 +0200

    chore(plotting): add plotting package __init__ exports

---
+++ ama_tlbx/src/ama_tlbx/plotting/__init__.py
@@ -0,0 +1,11 @@
+"""Plotting utilities for data visualization."""
+
+from .correlation_plots import plot_correlation_heatmap, plot_target_correlations, plot_top_correlated_pairs
+
+
+__all__ = [
+    # Correlation plots
+    "plot_correlation_heatmap",
+    "plot_target_correlations",
+    "plot_top_correlated_pairs",
+]
